30b7241ab124a1eea3461f1039179302df3b371e
angelicavelez <angelicashak@hotmail.com>
2023-08-07 12:55:20 -0500

    upload predict.py

---
+++ pipeline/predict.py
@@ -0,0 +1,15 @@
+from utils import model_evaluator, get_name_model
+
+def predict_model(model, X_train, y_train, X_valid, y_valid):
+
+    scores_valid  = model_evaluator(model, X_train, y_train, X_valid, y_valid)
+    roc_auc_valid = scores_valid.loc['test'].roc_auc
+
+    name = get_name_model(model)
+    print(f"Evaluation best model, roc_auc={roc_auc_valid:.2f}\n")
+
+    print(f'... executed predictions with best model {name}.\n')
+
+    return roc_auc_valid
+
+#====================================================================================================================
